71f1d663b49bac6d00dab9862c18cdf31286b481
lmrwork <59361885@qq.com>
2018-02-24 13:22:40 +0800

    init php

--- src/utils/refetch.js
+++ src/utils/refetch.js
@@ -25,7 +25,7 @@ export const save_amp = connect( props => {
   return {
     save_amp: data => ({
       postResponse: {
-        url: 'http://europe.chtcdn.com/info.php/info_amp/save_amp',
+        url: 'http://202.103.68.62:9000/info.php/info_amp/save_amp',
         method: 'POST',
         force: true,
         headers: {
@@ -49,7 +49,7 @@ export const load_amp = connect( props => {
   return {
     load_amp: icid => ({
       postResponse: {
-        url: 'http://europe.chtcdn.com/info.php/info_amp/load_amp',
+        url: 'http://202.103.68.62:9000/info.php/info_amp/load_amp',
         method: 'POST',
         force: true,
         headers: {
